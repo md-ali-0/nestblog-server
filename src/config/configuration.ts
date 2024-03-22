d081cc910e6afcf0e338fb962827ef1943ea1ad8
md-ali-0 <mohammad.98482@gmail.com>
2024-03-22 21:43:51 +0600

    nest js blog server

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -5,5 +5,5 @@ export default registerAs('dbConfig', () => ({
   dbPort: parseInt(process.env.DB_PORT, 10) || 3306,
   dbUser: process.env.DB_USER || 'root',
   dbPass: process.env.DB_PASS || '',
-  dbName: process.env.DB_NAME || 'my_database',
+  dbName: process.env.DB_NAME || 'test',
 }));
